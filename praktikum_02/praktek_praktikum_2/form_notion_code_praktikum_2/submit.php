0753a026e9ad7807087b90f4934b222044d5a9a3
Eko Muchamad Haryono <66320809+ekomh170@users.noreply.github.com>
2024-02-27 23:45:08 +0700

    2.1. melakukan commit pada latihan untuk kode di notion praktikum 2 di kelas saat pertemuan ke 2

---
+++ praktikum_02/praktek_praktikum_2/form_notion_code_praktikum_2/submit.php
@@ -0,0 +1,15 @@
+<?php
+  $nama = $_POST['nama'];
+  $matkul = $_POST['matkul'];
+  $nilai_uts = $_POST['nilai_uts'];
+  $nilai_uas = $_POST['nilai_uas'];
+  $nilai_tugas = $_POST['nilai_tugas'];
+
+  
+  echo "Nama: " . $nama . "<br>";
+  echo "Matkul: " . $matkul . "<br>";
+  echo "Nilai UTS: " . $nilai_uts . "<br>";
+  echo "Nilai UTS: " . $nilai_uas . "<br>";
+  echo "Nilai UTS: " . $nilai_tugas;
+
+?>
---
+++ praktikum_02/praktek_praktikum_2/form_notion_code_praktikum_2/switch.php
@@ -0,0 +1,29 @@
+<?php 
+
+$grade = "B";
+$predikat;
+
+switch ($grade) {
+	case 'A':
+		$predikat="Sangat Memuaskan";
+		break;
+	case "B":
+		$predikat="Memuaskan";
+		break;
+	case "C":
+		$predikat="Cukup";
+		break;
+	case "D":
+		$predikat="Kurang";
+		break;
+	case "E":
+		$predikat="Sangat Kurang";
+		break;
+	default:
+		$predikat="Tidak Ada";
+		break;
+}
+
+echo $predikat;
+
+?>
