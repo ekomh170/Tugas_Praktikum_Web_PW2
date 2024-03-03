6bdc59822fe069ed90cb62d8205ef3c52a58b97e
Eko Muchamad Haryono <66320809+ekomh170@users.noreply.github.com>
2024-03-03 21:37:03 +0700

    2.2. mengubah nama file sesuai dengan tugas 2

---
+++ praktikum_02/latihan_praktikum_mandiri_2/form_belanja.php
@@ -0,0 +1,32 @@
+<!-- Tugas Punya : Eko Muchamad Haryono -->
+<!-- TI - 02 -->
+<!-- Teknik Informatika -->
+<!-- Pemograman Web 2 (PW2) -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Belanja Online - Tugas Pertemuan 2</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h3>Form Belanja Online</h3>
+               
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
+
+<!-- Tugas Punya : Eko Muchamad Haryono -->
+<!-- TI - 02 -->
+<!-- Teknik Informatika -->
+<!-- Pemograman Web 2 (PW2) -->
