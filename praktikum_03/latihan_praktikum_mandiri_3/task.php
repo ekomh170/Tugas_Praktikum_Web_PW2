56ae14326033bf550f00b86fa70687f3f0586d5f
Eko Muchamad Haryono <66320809+ekomh170@users.noreply.github.com>
2024-03-05 13:33:37 +0700

    melakukan init praktikum mandiri 3

---
+++ praktikum_03/latihan_praktikum_mandiri_3/task.php
@@ -0,0 +1,59 @@
+<?php
+
+echo "<h3>Soal No 1 Looping I Love PHP</h3>";
+/* 
+Soal No 1 
+Looping I Love PHP
+Lakukan Perulangan (boleh for/while/do while) sebanyak 20 iterasi. Looping terbagi menjadi dua: Looping yang pertama Ascending (meningkat) 
+dan Looping yang ke dua menurun (Descending). 
+
+Output: 
+LOOPING PERTAMA
+            2 - I Love PHP
+            4 - I Love PHP
+            6 - I Love PHP
+            8 - I Love PHP
+            10 - I Love PHP
+            12 - I Love PHP
+            14 - I Love PHP
+            16 - I Love PHP
+            18 - I Love PHP
+            20- I Love PHP
+LOOPING KEDUA
+            20 - I Love PHP
+            18 - I Love PHP
+            16 - I Love PHP
+            14 - I Love PHP
+            12 - I Love PHP
+            10 - I Love PHP
+            8 - I Love PHP
+            6 - I Love PHP
+            4 - I Love PHP
+            2 - I Love PHP
+*/
+
+echo "Output: <br> <br>LOOPING PERTAMA: <br>";
+// Lakukan Looping Pertama Di Sini
+
+echo "<br>LOOPING KEDUA: <br>";
+// Lakukan Looping Kedua Di Sini
+
+
+echo "<h3>Soal No 2 Function Tentukan Nilai</h3>";
+/*
+Soal 2
+buatlah sebuah function bernama tentukan_nilai . Di dalam function tentukan_nilai yang menerima parameter 
+berupa integer. dengan ketentuan jika paramater integer lebih besar dari sama dengan 85 dan lebih kecil sama dengan 100 maka akan mereturn String “Sangat Baik” 
+Selain itu jika parameter integer lebih besar sama dengan 70 dan lebih kecil dari 85 maka akan mereturn string “Baik” selain itu jika parameter number lebih besar 
+sama dengan 60 dan lebih kecil dari 70 maka akan mereturn string “Cukup” selain itu maka akan mereturn string “Kurang”
+*/
+
+// Code function di sini
+
+// Hapus komentar di bawah ini untuk jalankan code
+// echo tentukan_nilai(98); //Sangat Baik
+// echo tentukan_nilai(76); //Baik
+// echo tentukan_nilai(67); //Cukup
+// echo tentukan_nilai(43); //Kurang
+
+?>
