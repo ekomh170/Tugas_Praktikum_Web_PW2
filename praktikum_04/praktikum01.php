0ae7404519cda70ab7aa9968623bb58eb5d88d3f
Eko Muchamad Haryono <66320809+ekomh170@users.noreply.github.com>
2024-03-16 11:42:07 +0700

    4.1. Menambahkan Konten Tugas Praktikum 1 & 2

--- praktikum_04/praktikum01.php
+++ praktikum_04/praktikum01.php
@@ -1,6 +1,21 @@
 <?php
 require_once 'header.php';
 require_once 'sidebar.php';
+
+// Buat array asosiatif nilai
+$nilai1 = ["id" => 1, "nim" => "01101", "uts" => 80, "uas" => 84, "tugas" => 78];
+$nilai2 = ["id" => 2, "nim" => "01102", "uts" => 90, "uas" => 80, "tugas" => 88];
+$nilai3 = ["id" => 3, "nim" => "01103", "uts" => 70, "uas" => 74, "tugas" => 70];
+$nilai4 = ["id" => 4, "nim" => "01104", "uts" => 88, "uas" => 95, "tugas" => 80];
+
+// Buat array multidimensi
+$kumpulan_nilai = [
+    $nilai1, 
+    $nilai2, 
+    $nilai3, 
+    $nilai4
+];
+
 ?>
 
 <!-- Content Wrapper. Contains page content -->
@@ -18,7 +33,6 @@ require_once 'sidebar.php';
 
   <!-- Main content -->
   <section class="content">
-
     <div class="container-fluid">
       <div class="row">
         <div class="col-12">
@@ -26,23 +40,47 @@ require_once 'sidebar.php';
                <!-- Default box -->
                <div class="card">
             <div class="card-header">
-              <h3 class="card-title">Judul</h3>
-
-              <div class="card-tools">
-                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                  <i class="fas fa-minus"></i>
-                </button>
-                <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-                  <i class="fas fa-times"></i>
-                </button>
-              </div>
+              <h3 class="card-title">Praktikum 1 Website - Eko Muchamad Haryono</h3>
             </div>
             <div class="card-body">
-              Start creating your amazing application!
+            <div class="container">
+            <h1 class="text-center display-4 mb-4">Daftar Nilai Siswa</h1>
+
+            <div class="table-responsive">
+                <table class="table table-bordered">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">No</th>
+                            <th scope="col">NIM</th>
+                            <th scope="col">UTS</th>
+                            <th scope="col">UAS</th>
+                            <th scope="col">Tugas</th>
+                            <th scope="col">Nilai Akhir</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            foreach ($kumpulan_nilai as $nilai): ?>
+
+                        <tr>
+                            <?php $nilai_akhir = ($nilai["uts"] + $nilai["uas"] + $nilai["tugas"]) / 3; ?>
+                            <td><?= $nilai["id"]?></td>
+                            <td><?= $nilai["nim"]?></td>
+                            <td><?= $nilai["uts"]?></td>
+                            <td><?= $nilai["uas"]?></td>
+                            <td><?= $nilai["tugas"]?></td>
+                            <td><?= number_format($nilai_akhir, 2, ",", ".")  ?></td>
+                        </tr>
+
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
             </div>
             <!-- /.card-body -->
             <div class="card-footer">
-              Footer
+            Praktikum 1 Website - Eko Muchamad Haryono
             </div>
             <!-- /.card-footer-->
           </div>
--- praktikum_04/praktikum02.php
+++ praktikum_04/praktikum02.php
@@ -22,31 +22,142 @@ require_once 'sidebar.php';
     <div class="container-fluid">
       <div class="row">
         <div class="col-12">
-               <!-- Default box -->
-               <div class="card">
+          <!-- Default box -->
+          <div class="card">
             <div class="card-header">
-              <h3 class="card-title">Judul</h3>
+              <h3 class="card-title">Praktikum 2 Website - Eko Muchamad Haryono</h3>
 
-              <div class="card-tools">
-                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                  <i class="fas fa-minus"></i>
-                </button>
-                <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-                  <i class="fas fa-times"></i>
-                </button>
-              </div>
+              
             </div>
             <div class="card-body">
-              Start creating your amazing application!
+              <div class="container">
+                <div class="row">
+                  <!-- Form Belanja -->
+                  <div class="col-md-6">
+                    <div class="card kombinasi-container">
+                      <h3 class="card-title text-center">Form Belanja Online</h3>
+                      <hr />
+                      <form action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
+                        <div class="form-group row">
+                          <label for="customer" class="col-md-4 col-form-labe fw-bold">Customer :</label>
+                          <div class="col-md-8">
+                            <input type="text" name="customer" id="customer" class="form-control" required>
+                          </div>
+                        </div>
+                        <div class="form-group">
+                          <label style="margin-right: 20%;" class="fw-bold">Pilih Produk : </label>
+                          <input type="radio" id="tv" name="produk" value="TV" required>
+                          <label for="tv">TV</label>
+                          <input type="radio" id="kulkas" name="produk" value="Kulkas" required>
+                          <label for="kulkas">Kulkas</label>
+                          <input type="radio" id="mesin-cuci" name="produk" value="Mesin Cuci" required>
+                          <label for="mesin-cuci">Mesin Cuci</label>
+                        </div>
+                        <div class="form-group row">
+                          <label for="jumlah" class="col-md-4 col-form-label fw-bold">Jumlah :</label>
+                          <div class="col-md-8">
+                            <input type="number" name="jumlah" id="jumlah" class="form-control" required>
+                          </div>
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-block">Kirim</button>
+                      </form>
+                    </div>
+                  </div>
+                  <!-- Daftar Harga -->
+                  <div class="col-md-6">
+                    <div class="card kombinasi-container">
+                      <table class="harga-table">
+                        <thead>
+                          <tr>
+                            <th class="text-center bg-primary" colspan="2">Daftar Harga Produk</th>
+                          </tr>
+                          <tr>
+                            <th class="text-center">Produk</th>
+                            <th class="text-center">Harga</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td class="text-center">TV</td>
+                            <td class="text-center">Rp. 2.000.000</td>
+                          </tr>
+                          <tr>
+                            <td class="text-center">Kulkas</td>
+                            <td class="text-center">Rp. 3.500.000</td>
+                          </tr>
+                          <tr>
+                            <td class="text-center">Mesin Cuci</td>
+                            <td class="text-center">Rp. 2.800.000</td>
+                          </tr>
+                          <tr>
+                            <th colspan="2" class="text-center text-danger bg-primary">Harga dapat berubah
+                              sewaktu-waktu</th>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+
+                <!-- Detail Pembelian -->
+                <?php
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                  $customer = $_POST["customer"];
+                  $produk = $_POST["produk"];
+                  $jumlah = $_POST["jumlah"];
+
+                  echo '
+          <div id="detailPembelianPrint">
+              <div class="row detail-pembelian">
+                  <div class="col-md-12">
+                      <div class="card kombinasi-container">
+                          <h3 class="card-title text-center">Detail Pembelian</h3>
+                          <hr />
+                          <div>
+                              <p><strong>Nama Customer :</strong>' . $customer . '</p>
+                              <p><strong>Produk Pilihan : </strong>' . $produk . '</p>
+                              <p><strong>Jumlah Beli : </strong>' . $jumlah . '</p>
+                              <p><strong>Total Belanja : </strong> Rp. ';
+
+                  // Hitung total belanja
+                  $total_belanja = 0;
+                  if ($produk == "TV") {
+                    $harga_produk = 2000000;
+                    $total_belanja = $harga_produk * $jumlah;
+                  } elseif ($produk == "Kulkas") {
+                    $harga_produk = 3500000;
+                    $total_belanja = $harga_produk * $jumlah;
+                  } elseif ($produk == "Mesin Cuci") {
+                    $harga_produk = 2800000;
+                    $total_belanja = $harga_produk * $jumlah;
+                  }
+
+                  // Format total belanja menjadi format mata uang Rupiah
+                  $total_belanja_rp = number_format($total_belanja, 0, ',', '.');
+
+                  echo $total_belanja_rp . '</p>
+                          <p><strong>Harga Satuan Barang : </strong>' . number_format($harga_produk, 0, ',', '.') . '</p>
+                        </div>
+                        <button id="cetakBtn" class="btn btn-primary btn-block mt-3">Cetak Detail Pembelian</button>
+    
+                    </div>
+                </div>
+            </div>
+        </div>';
+                }
+
+                ?>
+                
+              </div>
             </div>
             <!-- /.card-body -->
             <div class="card-footer">
-              Footer
+            Praktikum 2 Website - Eko Muchamad Haryono
             </div>
             <!-- /.card-footer-->
           </div>
           <!-- /.card -->
-          
+
         </div>
       </div>
     </div>
