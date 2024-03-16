3972e5535181277d66b77e39e7fe3883f46a9bbc
Eko Muchamad Haryono <66320809+ekomh170@users.noreply.github.com>
2024-03-16 12:01:49 +0700

    4.1. Menambahkan Menu Profile Kreator di Sidebar & Memperbaiki Tampilan Profile Kreator

--- praktikum_04/index.php
+++ praktikum_04/index.php
@@ -18,38 +18,63 @@ require_once 'sidebar.php';
 
   <!-- Main content -->
   <section class="content">
-
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-12">
-          <!-- Default box -->
-          <div class="card">
-            <div class="card-header">
-              <h3 class="card-title">Judul</h3>
-
-              <div class="card-tools">
-                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                  <i class="fas fa-minus"></i>
-                </button>
-                <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-                  <i class="fas fa-times"></i>
-                </button>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <!-- Default box -->
+        <div class="card">
+          <div class="card-header">
+            <h3 class="card-title">Dashboard Website - Eko Muchamad Haryono</h3>
+            <div class="card-tools">
+              <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                <i class="fas fa-minus"></i>
+              </button>
+              <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+                <i class="fas fa-times"></i>
+              </button>
+            </div>
+          </div>
+          <div class="card-body">
+            <div class="container">
+              <h4 class="text-center mt-4">Selamat Datang Di Aplikasi Website Pengumpulan Tugas Pemrograman Web 2</h4>
+              <div class="row justify-content-center mt-5">
+                <div class="col-md-6">
+                  <div class="card">
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="col-md-4">
+                        <h5 class="card-title">Informasi Mahasiswa</h5>
+                          <div class="text-center">
+                            <img src="dist/img/mypicture/profile/profile.png" class="img-fluid" alt="Profile Image">
+                          </div>
+                        </div>
+                        <div class="col-md-8">
+                          <ul class="list-group list-group-flush">
+                            <li class="list-group-item"><strong>Nama :</strong> Eko Muchamad Haryono</li>
+                            <li class="list-group-item"><strong>NIM :</strong> 0110223079</li>
+                            <li class="list-group-item"><strong>Kelas :</strong> TI02</li>
+                            <li class="list-group-item"><strong>Program Studi :</strong> Teknik Informatika</li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-            <div class="card-body">
-              Start creating your amazing application!
-            </div>
-            <!-- /.card-body -->
-            <div class="card-footer">
-              Footer
-            </div>
-            <!-- /.card-footer-->
           </div>
-          <!-- /.card -->
+          <!-- /.card-body -->
+          <div class="card-footer">
+            Dashboard Website - Eko Muchamad Haryono
+          </div>
+          <!-- /.card-footer-->
         </div>
+        <!-- /.card -->
       </div>
     </div>
-  </section>
+  </div>
+</section>
+
   <!-- /.content -->
 </div>
 <!-- /.content-wrapper -->
--- praktikum_04/sidebar.php
+++ praktikum_04/sidebar.php
@@ -35,6 +35,14 @@
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
+               <li class="nav-item">
+                    <a href="index.php" class="nav-link">
+                        <i class="nav-icon fas fa-user-alt"></i>
+                        <p>
+                            Profile Kreator
+                        </p>
+                    </a>
+                </li>
                 <li class="nav-item">
                     <a href="#" class="nav-link">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
