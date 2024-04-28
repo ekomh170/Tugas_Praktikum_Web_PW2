502ca1c8e2634c2859e61ab72b25459e073bdc2c
Eko Muchamad Haryono <66320809+ekomh170@users.noreply.github.com>
2024-04-28 14:57:02 +0700

    7.2. Melakukan Update Pengumpulan Progress Pertemuan 7

--- praktikum_07/periksa/header.php
+++ praktikum_07/periksa/header.php
@@ -7,13 +7,14 @@
     <title>Halaman Data Periksa - Project 01 - Eko Muchamad Haryono</title>
 
     <!-- Favicon, Apple Touch Icon, Android Chrome 192 & 512, Favicon 16 & 32, Site.Web Manifest  -->
-    <link rel="icon" href="../dist/img/favicon/favicon.ico" type="image/x-icon" />
-    <link rel="apple-touch-icon" sizes="180x180" href="../dist/img/favicon/apple-touch-icon.png" />
-    <link rel="icon" type="image/png" sizes="192x192" href="../dist/img/favicon/android-chrome-192x192.png" />
-    <link rel="icon" type="image/png" sizes="512x512" href="../dist/img/favicon/android-chrome-512x512.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="../dist/img/favicon/favicon-16x16.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="../dist/img/favicon/favicon-32x32.png" />
-    <link rel="manifest" href="../dist/img/favicon/site.webmanifest" />
+    <link rel="icon" href="dist/img/favicon/favicon.ico" type="image/x-icon" />
+    <link rel="apple-touch-icon" sizes="180x180" href="dist/img/favicon/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="192x192" href="dist/img/favicon/android-chrome-192x192.png" />
+    <link rel="icon" type="image/png" sizes="512x512" href="dist/img/favicon/android-chrome-512x512.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="dist/img/favicon/favicon-16x16.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="dist/img/favicon/favicon-32x32.png" />
+    <link rel="manifest" href="dist/img/favicon/site.webmanifest" />
+
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
