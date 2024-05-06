2c2cab1298d9b89bb0310ba149e59993c5a8f907
Eko Muchamad Haryono <66320809+ekomh170@users.noreply.github.com>
2024-05-06 15:27:00 +0700

    9.1. Melakukan Upload Laravel Praktikum 9

---
+++ praktikum_09/resources/views/nilai.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Nilai</title>
+</head>
+
+<body>
+    @php
+        $nilai = 90;
+    @endphp
+    {{-- struktur kendali if --}}
+    @if ($nilai >= 90)
+        <h1>Nilai Saya A</h1>
+    @endif
+    <h1>Nilai Saya {{ $nilai }} </h1>
+</body>
+
+</html>
---
+++ praktikum_09/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Buat route kabar dengan view kondisi
+Route::get('/kabar', function () {
+    return view('kondisi');
+});
+
+// Buat route nilai dengan view nilai
+Route::get('/nilai', function () {
+    return view('nilai');
+});
+
+// Buat route ke pasien dengan view pasien
+Route::get('/pasien', function () {
+    return view('pasien');
+});
