d04cf227e846eb410c08ac3bcf43dea93007f41c
Eko Muchamad Haryono <66320809+ekomh170@users.noreply.github.com>
2024-05-14 23:36:04 +0700

    10.2. Set Up Controller & Route & File Awal Index Admin Untuk AdminController

--- praktikum_10/routes/web.php
+++ praktikum_10/routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/kabar', function () {
+    return view('kondisi');
+});
+
+Route::get('/pasien', function () {
+    return view('pasien');
+});
+
+Route::get('/admin', [AdminController::class, 'index']);
