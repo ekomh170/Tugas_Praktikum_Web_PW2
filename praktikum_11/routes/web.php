999db3e3f9326a148fda1a39fbc7888a6ff91831
Eko Muchamad Haryono <66320809+ekomh170@users.noreply.github.com>
2024-05-23 17:05:04 +0700

    11.5. Updated Pada Praktikum 11 Menyesuaikan Dengan Menambahkan Fitur Praktikum 9 - 11 Yang Berkaitan Tentang Laravel 11

--- praktikum_11/routes/web.php
+++ praktikum_11/routes/web.php
@@ -20,16 +20,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
+Route::get('/', [AdminController::class, 'index']);
+
+// Buat route kabar dengan view kondisi
 Route::get('/kabar', function () {
     return view('kondisi');
 });
 
+// Buat route nilai dengan view nilai
 Route::get('/nilai', function () {
     return view('nilai');
+});
+
+// Buat route ke pasien dengan view pasien
+Route::get('/pasien', function () {
+    return view('pasien');
 });
 
 Route::get('/admin', [AdminController::class, 'index']);
